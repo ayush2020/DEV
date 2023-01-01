3af9a9c8bb7ad4ac2919088cbf412824d0cabe42
Ayush Rai <ayushayushravi@gmail.com>
2023-01-01 12:43:30 +0530

    firtcommit

---
+++ client/src/Component/pages/ListProper/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function List() {
+  return (
+    <div>
+      <h1>There are only 10 property are avaible and climbling hight</h1>
+    </div>
+  )
+}
+
+export default List
